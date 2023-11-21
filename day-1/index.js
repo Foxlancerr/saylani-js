336bb0577d7c6d619bff3c25b7b02a280e4180ae
M-Asimm <kpkprince55persia@gmail.com>
2023-11-21 18:16:46 +0500

    arithematic operators start

---
+++ day-1/index.js
@@ -0,0 +1,37 @@
+let name = "mark";
+console.log(name);
+
+// var country = "Pakistan";
+// var continent = "Asia";
+// var population = 25;
+
+let country;
+let continent;
+let population;
+
+country = "Pakistan";
+continent = "Asia";
+population = 25;
+
+console.log("My country name: " + country + " and the continent: " + continent + " and population: " + population + " Millions");
+
+console.log(country, continent, population);
+
+const Apple = "Apple";
+const apple = "apple";
+console.log(apple === Apple);
+
+const num1 = prompt("Enter num1")
+const num2 = prompt("Enter num2")
+
+const add = Number(num1) + Number(num2);
+const sub = parseInt(num1) - parseInt(num2);
+const divide = parseInt(num1) / parseInt(num2);
+const multiply = parseInt(num1) * parseInt(num2);
+const modulas = parseInt(num1) % parseInt(num2);
+
+console.log("the addition is " + add)
+console.log("the sub is " + sub)
+console.log("the multiply is " + multiply)
+console.log("the divide is " + divide)
+console.log("the modulas is " + modulas)
---
+++ day-2/index.js
@@ -0,0 +1,33 @@
+// let x = 4;
+// let y = 10;
+
+// console.log(x+y);
+// console.log(x-y);
+
+// console.log(6+7-2+6+4-3+2);
+// 13-2+6+4-3+2
+// 11+6+4-3+2
+// 17+4-3+2
+// 21-3+2
+// 18+2
+// 20
+// console.log(10%3);
+// console.log(10/3);
+
+// let num1 = prompt("enter num1")
+// let num2 = prompt("enter num2")
+
+// let modulas = Number(num1) % Number(num2);
+// let divide = Number(num1) / Number(num2);
+// let multiply = Number(num1) * Number(num2);
+// console.log(modulas);
+// console.log(divide);
+// console.log(multiply);
+
+// if your country population split in half, and each half contain half values, how many people would live in each half?
+let myCountryPopulation = 250000000;
+let halfPopulation = myCountryPopulation / 2;
+console.log(halfPopulation);
+let increasePopulationByOne = halfPopulation + 1;
+console.log(increasePopulationByOne);
+
